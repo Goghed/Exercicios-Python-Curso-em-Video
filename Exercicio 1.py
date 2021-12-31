987fd4e0790654ed485d602fb3277be231057931
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2021-12-31 16:24:07 -0300

    commit 1

---
+++ Exercicio 1.py
@@ -0,0 +1,9 @@
+# Crie um programa que escreva "Olá, Mundo!" na tela.
+
+print('Olá, Mundo!')
+
+# ou de outra forma.
+
+mensagem = 'Olá, Mundo!'
+
+print(mensagem)
---
+++ Exercicio 2.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Digite o seu nome: ')
+
+print('Olá, é um prazer conhece-lo, {}!' .format(nome))
